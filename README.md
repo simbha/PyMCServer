96c32484eb2561186e588f95e6140cd11d7952c5
a646767 <mc@gblox.org>
2012-05-15 01:07:16 +0000

    Logging

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 PyMCServer
 ==========
+
+PyMCServer is a web based Minecraft administration console.
+
+Installation
+============
+
+PyMCServer requires no installation. Simply run `./start.sh` to start PyMCServer.
--- src/pymcserver/main.py
+++ src/pymcserver/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2
 
 import sys
-import os
 
 if sys.version_info.major != 2:
     print("PyMCServer requires Python 2.x to run.")
@@ -17,18 +16,26 @@ __datadir = "data"
 
 from pymcserver.server import WebServer
 import logging
+import os
+
+def mkdir(path):
+    if not os.path.exists(__datadir):
+        os.makedirs(__datadir)
 
 def initServer():
-    if not os.path.exists(__datadir):
-        os.mkdir(__datadir)
-    
+    # Setup logging
     sh = logging.StreamHandler()
     sh.setLevel(logging.DEBUG)
     sh.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] - %(message)s"))
     
+    fh = logging.FileHandler(os.path.join(__datadir, "webserver.log"))
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] - %(message)s"))
+    
     __log = logging.getLogger("PyMCServer")
     __log.setLevel(logging.DEBUG)
     __log.addHandler(sh)
+    __log.addHandler(fh)
     __log.info("W** SUCKS")
     
     __server = WebServer("127.0.0.1", 8099)
