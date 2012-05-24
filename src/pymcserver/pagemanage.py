42b7f2fdc74d154234954939b2eb85318a5c7808
a646767 <mc@gblox.org>
2012-05-24 01:59:01 +0000

    It took 3 hours to figure out how to get sidebars with a menubar dev...

--- src/pymcserver/pagemanage.py
+++ src/pymcserver/pagemanage.py
@@ -1,10 +1,22 @@
+pagecode = """<table style="border-collapse: collapse; height: 100%; padding-top: 28px">
+<tr>
+<td style="background: green; width: 240px">1</td>
+<td style="background: black">2</td>
+</tr>
+</table>
+"""
+
+#pagecode = """
+#<div style="background: green; min-height: 100%">w** SUCKS
+#</div>"""
+
 def handlePage(handler, res, path):
     if path == "/":
         res.code = 200
         res.endHeaders()
         handler.wfile.write(handler.getServer().pageComponents["header"]())
         handler.wfile.write(handler.getServer().pageComponents["menubar"](handler))
-        handler.wfile.write("<p>w** SUCKS</p>")
+        handler.wfile.write(pagecode)
         handler.wfile.write(handler.getServer().pageComponents["footer"]())
     else:
         res.code = 404
