a9c730d8aaf7fa74b748d1876d6f8f9fea19e6f0
a646767 <mc@gblox.org>
2012-05-25 03:34:04 +0000

    i want to smash that w**

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ if sys.version_info[0] != 2:
     print("PyMCServer requires Python 2.x to run.")
     exit(1)
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
 
 os.chdir(os.path.dirname(__file__))
 import pymcserver.server
