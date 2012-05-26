83168dc36221e8de29cc042fd7d8f72b05eb44dd
A646767 <mc@gblox.org>
2012-05-25 18:42:38 -0700

    No longer need a meta refresh page for logout

--- src/pymcserver/pagelogout.py
+++ src/pymcserver/pagelogout.py
@@ -1,19 +1,9 @@
-postbox = """<div class="centerBox">
-<h2>Logging out...</h2>
-<p style="margin-top: 8px">Go <a href="/">here</a> you didn't redirect.</p>
-</div>
-"""
-
 def handlePage(handler, res, path):
-    # Why a meta refresh instead of a redirect. Well, chrome
-    # seems to break the "logout" button and instead puts you
-    # to the manage page again, instead of logging out, so
-    # I used a meta refresh as a hack.
     if path == "/":
         handler.getSession().user = None
-        handler.wfile.write(handler.getServer().pageComponents["header"](extraHead="""<meta http-equiv="refresh" content="1; url=/">\n"""))
-        handler.wfile.write(postbox)
-        handler.wfile.write(handler.getServer().pageComponents["footer"]())
+        res.code = 301
+        res.headers["Location"] = "/"
+        res.endHeaders()
     else:
         res.code = 404
         handler.sendErrorPage(res)
