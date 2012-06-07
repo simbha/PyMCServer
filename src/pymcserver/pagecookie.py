3eca21dd17ce4fe3775b28b07d97c69766aaefc4
A646767 <mc@gblox.org>
2012-06-06 20:50:02 -0700

    No more /cookies

--- src/pymcserver/pagecookie.py
+++ src/pymcserver/pagecookie.py
@@ -1,6 +1,9 @@
 # List all cookies, this was used for testing. Should be moved to a server command.
 def handlePage(handler, res, path):
-    res.code = 200
+    res.code = 404
+    res.endHeaders()
+    return
+    '''res.code = 200
     res.headers["Content-Type"] = "text/plain"
     res.endHeaders()
     wfile = res.getWFile()
@@ -9,4 +12,4 @@ def handlePage(handler, res, path):
     for key, value in handler.getServer().allSessions.iteritems():
         wfile.write(key + ":\n")
         for k, v in value.__dict__.iteritems():
-            wfile.write("    %s: %s\n" % (k, v))
+            wfile.write("    %s: %s\n" % (k, v))'''
